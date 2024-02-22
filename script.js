7c7c996621832f6bb473aa6e678a163fec30911f
xikmatilla6229 <xikmatilla996@gmail.com>
2024-02-22 20:09:59 +0500

    git

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let a=prompt('Qaysi shahardansiz?')
-let b=prompt('Ismingiz nima?')
-let d=prompt('Yoshingiz nechida?')
-let c=prompt('Telefon raqamingiz?')
+let a=prompt('Ismingizni kiriting')
+let b=prompt('Familiyangiznizni kiriting')
+let d=prompt('Yoshingizni kiriting')
+let c=prompt('Yoqtirgan faninggizni kiriting')
+let e=prompt('Yashaydigan Tumaningizni kiriting')
 
-
+alert('malumotingizni kiritganingiz uchun raxmat')
 console.log(a,b,d,c);
